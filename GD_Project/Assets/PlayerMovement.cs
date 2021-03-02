3877f829703cc22585d4e7228330586354843596
costinaioana18 <61825038+costinaioana18@users.noreply.github.com>
2021-03-02 15:10:09 +0200

    Added shooting feature

--- GD_Project/Assets/PlayerMovement.cs
+++ GD_Project/Assets/PlayerMovement.cs
@@ -8,8 +8,10 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
     public Animator animator;
+    public Camera cam;
 
     Vector2 movement;
+    Vector2 mousePos;
     // Update is called once per frame
     void Update()
     {
@@ -20,11 +22,18 @@ public class PlayerMovement : MonoBehaviour
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        mousePos= cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     private void FixedUpdate()
     {
         //Movement handled here
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDir=mousePos-rb.position;
+        float angle=Mathf.Atan2(lookDir.y,lookDir.x)* Mathf.Rad2Deg-90f;
+        rb.rotation=angle;
+
     }
 }
